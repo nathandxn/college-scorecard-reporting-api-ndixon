1523967c5fff61f50e369efb0e8fe247722acb00
nathandxn <nathan.dixon@snyk.io>
2025-02-04 14:23:30 -0500

    style: f ormat eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
+// latest rules: https://eslint.org/docs/latest/rules/
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
