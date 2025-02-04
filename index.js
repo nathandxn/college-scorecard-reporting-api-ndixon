fa5265b7189c407c55f0951e3ed3730830f91fa9
nathandxn <nathan.dixon@snyk.io>
2025-02-04 16:44:15 -0500

    chore: test husky

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log("API running on port 3000"));
+app.listen(3000, () => console.log('API running on port 3000'));
 
 app.get('/', (req, res) => res.json('My API is running'));
