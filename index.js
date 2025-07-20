6c3b65b9cfb152e2870184c2a241583badd412fb
nathandxn <nathan.dixon@snyk.io>
2025-07-20 00:23:53 -0400

    feat: update test text

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = express();
 
 app.listen(3000, () => console.log('API running on port 3000'));
 
-app.get('/', (req, res) => res.json('This test API is now running'));
+app.get('/', (req, res) =>
+  res.json('This test API is now running successfully...'),
+);
