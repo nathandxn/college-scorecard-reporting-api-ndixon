ff2b6e65059cedd74a1f579ac22f7ac100927dc8
nathandxn <nathan.dixon@snyk.io>
2025-07-21 22:42:22 -0400

    chore: add test access control issue

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/admin', (req, res) => {
+  res.send('Welcome to the Admin Panel!');
+});
+
 app.listen(3000, () => console.log('API running on port 3000'));
 
 app.get('/', (req, res) =>
