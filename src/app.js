287a89923af78cf25c28fe5587bd5f5c2dc027f2
nathandxn <nathandxn@gmail.com>
2026-05-31 18:12:42 -0400

    feat: add schools routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import schoolsRouter from './routes/schools.js';
 
 const app = express();
 
 app.get('/', (req, res) => res.json('This test API is now running...'));
+
+app.use(schoolsRouter);
+
+app.use((err, req, res, _next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal server error' });
+});
 
 export default app;
